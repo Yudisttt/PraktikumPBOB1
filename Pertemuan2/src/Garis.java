5012f8094ffe2d377634f66529591e860dc2e6db
Yudisttt <ida.yudist@gmail.com>
2026-03-03 23:54:03 +0700

    Buat File garis

---
+++ Pertemuan2/src/Garis.java
@@ -0,0 +1,11 @@
+/* Nama File    : Garis.java
+ * Deskripsi    : berisi atribut dan method dalam class Garis
+ * Pembuat      : Ida Bagus Ngurah Yudistira K
+ * Tanggal      : 23 Maret 2026
+ * 
+ */
+
+
+public class Garis {
+    
+}
---
+++ Pertemuan2/src/MGaris.java
@@ -0,0 +1,10 @@
+/* Nama File    : MGaris.java
+ * Deskripsi    : berisi Main Garis
+ * Pembuat      : Ida Bagus Ngurah Yudistira K
+ * Tanggal      : 3 Maret 2026
+ */
+
+
+public class MGaris {
+    
+}
