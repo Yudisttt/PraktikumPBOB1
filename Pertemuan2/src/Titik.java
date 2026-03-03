1a9e27c0fedd348495fa0b358e6d8949e1a1195c
Yudisttt <ida.yudist@gmail.com>
2026-03-04 00:16:14 +0700

    Pembuatan isi garis dan Mgaris

--- Pertemuan2/src/Titik.java
+++ Pertemuan2/src/Titik.java
@@ -49,7 +49,7 @@ public class Titik {
     //menggeser nilai absis dan ordinat titik masing masing sejauh x dan y
     void geser(double x, double y){
         absis = absis + x;
-        ordinat = ordinat + x;
+        ordinat = ordinat + y;
     }
 
     void printcountertitik(){
