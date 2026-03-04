13997b554686aabb897e2ffd1dbde7b517c1c89e
Yudisttt <ida.yudist@gmail.com>
2026-03-04 22:19:16 +0700

    Coba pertama MHS

---
+++ Pertemuan3/src/Dosen.java
@@ -0,0 +1,62 @@
+public class Dosen {
+
+    /*ATRIBUT*/
+
+    private String nip;
+    private String nama;
+    private String prodi;
+
+    /* METHOD*/
+    //SETTER
+    public void setNipDosen(String nip){
+        this.nip = nip;
+    }
+    public void setNamaDosen(String nama){
+        this.nama = nama;
+    }
+    public void setProdiDosen(String prodi){
+        this.prodi = prodi;
+    }
+
+    //GETTER
+    public String getNIPDosen(){ //Mengembalikan NIP Dosen
+        return nip;
+    }
+
+    public String getNamaDosen(){ //Mengembalikan Nama Dosen
+        return nama;
+    }
+
+    public String getProdiDosen(){ //Mengembalikan Prodi Dosen
+        return prodi;
+    }
+
+
+    //Konstruktor Dosen Tanpa Parameter
+    public Dosen(){
+        
+    }
+
+    //Konstruktor Dosen Dengan Parameter
+    public Dosen(String nip,String nama,String prodi){
+        this.nip = nip;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+
+
+    public void printDetailDosen(){
+        System.out.println("NIP: " + nip);
+        System.out.println("Nama: " + nama);
+        System.out.println("Prodi: " + prodi);
+    }
+
+    @Override
+    public String toString() {
+    return "NIP: " + nip +
+           ", Nama: " + nama +
+           ", Prodi: " + prodi;
+    }
+
+
+}
