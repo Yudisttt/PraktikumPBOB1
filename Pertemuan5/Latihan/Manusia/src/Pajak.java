6dc5ba0e11260f4fa566e2123a1a45c60d565142
Yudisttt <ida.yudist@gmail.com>
2026-03-25 19:55:02 +0700

    commit pertemuan 5 latihan dan bangundatar

---
+++ Pertemuan5/Latihan/Manusia/src/Pajak.java
@@ -0,0 +1,13 @@
+/*
+File : Pajak.java
+Deskripsi : Interface yang berisi kontrak perhitungan pajak
+Nama : Ida Bagus Ngurah Yudistira K
+NIM  : 24060124140128
+Tanggal : 24 Maret 2026
+*/
+
+public interface Pajak {
+
+    // Method untuk menghitung pajak
+    public double hitungPajak();
+}
